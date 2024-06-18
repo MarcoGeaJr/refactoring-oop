26721e0c8223f72dd12a662c18e41c9380af3913
Marco Gea <69200078+iwtapcgmr@users.noreply.github.com>
2024-06-18 15:20:56 -0300

    feat: separation of data and business rules

--- Refactoring/BasketDiscounts/Solution/DiscountApplierFactory.cs
+++ Refactoring/BasketDiscounts/Solution/DiscountApplierFactory.cs
@@ -2,9 +2,27 @@
 
 public class DiscountApplierFactory
 {
+	private readonly IDiscountRuleRepository _discountRulesRepository;
+
+	public DiscountApplierFactory(IDiscountRuleRepository discountRulesRepository)
+	{
+		_discountRulesRepository = discountRulesRepository;
+	}
+
 	public DiscountApplier Build()
 	{
-		List<IDiscountStrategy> discounts = [new DiscountPerProduct(), new DiscountPerAmount()];
+		List<IDiscountStrategy> discounts = [];
+
+		List<DiscountPerProductRule> discountPerProductRules = _discountRulesRepository.GetAllDiscountPerProductRules();
+		foreach (var rule in discountPerProductRules)
+			discounts.Add(new DiscountPerProduct(rule.Products, rule.Discount));
+
+		List<DiscountPerAmountRule> discountPerAmountRules = _discountRulesRepository.GetAllDiscountPerAmountRules();
+		foreach (var rule in discountPerAmountRules)
+			discounts.Add(new DiscountPerAmount(rule.MinimumAmount, rule.MaximumAmount,
+												rule.MinimumQuantity, rule.MaximumQuantity,
+												rule.Discount));
+
 		return new(discounts);
 	}
 }
---
+++ Refactoring/BasketDiscounts/Solution/IDiscountRuleRepository.cs
@@ -0,0 +1,22 @@
+﻿namespace Refactoring.BasketDiscounts.Solution;
+
+public interface IDiscountRuleRepository
+{
+	List<DiscountPerProductRule> GetAllDiscountPerProductRules();
+	List<DiscountPerAmountRule> GetAllDiscountPerAmountRules();
+}
+
+public class DiscountPerProductRule
+{
+	public List<string> Products { get; set; }
+	public double Discount { get; set; }
+}
+
+public class DiscountPerAmountRule
+{
+	public double MinimumAmount { get; set; }
+	public double MaximumAmount { get; set; }
+	public int MinimumQuantity { get; set; }
+	public int MaximumQuantity { get; set; }
+	public double Discount { get; set; }
+}
