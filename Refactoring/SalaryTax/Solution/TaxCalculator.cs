83e46ba9efb91b717894e797af6a4e3c57a2b02b
Marco Gea <69200078+iwtapcgmr@users.noreply.github.com>
2024-06-19 17:00:28 -0300

    feat: calculate tax from tac calculation strategy created by a resolver

--- Refactoring/SalaryTax/Solution/TaxCalculator.cs
+++ Refactoring/SalaryTax/Solution/TaxCalculator.cs
@@ -1,21 +1,11 @@
 ﻿namespace Refactoring.SalaryTax.Solution;
 
-public class TaxCalculator
+public class TaxCalculator(TaxCalculationResolver taxCalculationResolver)
 {
 	public double CalculateTax(Employee employee)
 	{
-		if (Role.DEVELOPER.Equals(employee.Role))
-		{
-			return new ThresholdBasedTaxCalculationStrategy(3000, 0.8, 0.9).Calculate(employee);
-		}
+		ITaxCalculationStrategy calculationStrategy = taxCalculationResolver.ResolveForRole(employee.Role);
 
-		if (Role.DBA.Equals(employee.Role) || Role.TESTER.Equals(employee.Role))
-		{
-			return new ThresholdBasedTaxCalculationStrategy(2000, 0.75, 0.85).Calculate(employee);
-		}
-
-		// ... and many more ...
-
-		throw new InvalidOperationException("invalid employee");
+		return calculationStrategy.Calculate(employee);
 	}
 }
