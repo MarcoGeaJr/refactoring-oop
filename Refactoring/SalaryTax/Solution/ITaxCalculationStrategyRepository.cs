d7ff9752fd3750bf2d03fd09ed3bc9a69e0e982e
Marco Gea <69200078+iwtapcgmr@users.noreply.github.com>
2024-06-19 16:59:59 -0300

    feat: implement resolver to create a calculation strategy from database data

---
+++ Refactoring/SalaryTax/Solution/ITaxCalculationStrategyRepository.cs
@@ -0,0 +1,13 @@
+﻿namespace Refactoring.SalaryTax.Solution;
+
+public class TaxValues
+{
+	public double Threshold { get; private set; }
+	public double AboveTax { get; private set; }
+	public double BelowTax { get; private set; }
+}
+
+public interface ITaxCalculationStrategyRepository
+{
+	TaxValues GetByRole(Role role);
+}
---
+++ Refactoring/SalaryTax/Solution/TaxCalculationResolver.cs
@@ -0,0 +1,13 @@
+﻿namespace Refactoring.SalaryTax.Solution;
+
+public sealed class TaxCalculationResolver(ITaxCalculationStrategyRepository repository)
+{
+	public ITaxCalculationStrategy ResolveForRole(Role role)
+	{
+		TaxValues taxValues = repository.GetByRole(role);
+
+		return new ThresholdBasedTaxCalculationStrategy(taxValues.Threshold,
+														taxValues.AboveTax,
+														taxValues.BelowTax);
+	}
+}
