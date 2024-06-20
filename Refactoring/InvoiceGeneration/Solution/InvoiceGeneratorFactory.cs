5c3a31d73510111d89c2bb798d200adb14291c59
Marco Gea <69200078+iwtapcgmr@users.noreply.github.com>
2024-06-20 14:11:48 -0300

    feat: invoice generator factory

---
+++ Refactoring/InvoiceGeneration/Solution/InvoiceGeneratorFactory.cs
@@ -0,0 +1,13 @@
+﻿namespace Refactoring.InvoiceGeneration.Solution;
+
+public class InvoiceGeneratorFactory(IInvoiceRepository invoiceRepository, EmailSender sender)
+{
+	public InvoiceGenerator Build()
+	{
+		return new(new List<IInvoiceGeneratedAction>()
+		{
+			new PersistGeneratedInvoiceAction(invoiceRepository),
+			new SendGeneratedInvoiceEmail(sender)
+		});
+	}
+}
